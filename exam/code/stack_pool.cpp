173afef9503be17f365335b878e1d96e7241f5ae
Francesc0rtu <francescortu@live.it>
2022-02-26 23:31:00 +0100

    update

--- exam/code/stack_pool.cpp
+++ exam/code/stack_pool.cpp
@@ -1,18 +1,18 @@
 #include<vector>
 #include<iostream>
+// #include<memory>
 
  template <typename P, typename S, typename T>
  class _iterator{
-   P *ptr_stack_pool;
+   P* ptr_stack_pool;
    S stack;
   public:
    using value_type = T;
    using reference = value_type&;
    using pointer = value_type*;
 
-  _iterator(P* ptr, S stack): ptr_stack_pool{ptr}, stack{stack} {
-      // add some exception
-   }
+  _iterator(P* ptr, S stack): ptr_stack_pool{ptr}, stack{stack} {}
+
   reference operator*() const {return ptr_stack_pool -> value(stack);}
 
    _iterator& operator++() {  // pre-increment
@@ -32,7 +32,6 @@
   friend bool operator!=(const _iterator& x, const _iterator& y) {
     return !(x == y);
   }
-
  };
 
 
@@ -52,108 +51,109 @@ class stack_pool{
   const node_t& node(stack_type x) const noexcept { return pool[x-1]; }
 
   template <typename S>
-  stack_type _push(const S&& val, stack_type head){
+  stack_type _push(S&& val, stack_type head){
     if(empty(free_nodes) == true){
-      node_t _new = node_t();
-      _new.value = std::move(val); //why not std::forwaRD()?
-      _new.next = head;
-      pool.push_back(std::move(_new));
+      pool.push_back({std::forward<S>(val),head});
       return pool.size(); 
     }else{
       stack_type new_head{free_nodes};
       free_nodes = next(free_nodes);
-      next(new_head) = std::move(head);
-      value(new_head) = std::move(val);
+      next(new_head) = head;
+      value(new_head) = std::forward<S>(val);
       return new_head;
     }
 
   }
  public:
-  stack_pool(): free_nodes{end()} {};
-  explicit stack_pool(size_type n): free_nodes{end()} {
-    reserve(n); //funziona anche senza il this, perchè?
-  } // reserve n nodes in the pool
+
+  stack_pool() : free_nodes{end()} {};
+  explicit stack_pool(size_type n): free_nodes{end()} { reserve(n);  } // reserve n nodes in the pool
+
+  ~stack_pool() = default;
+
+
 
   using iterator = _iterator< stack_pool, stack_type, value_type>;
-  using const_iterator = _iterator< stack_pool, stack_type, const value_type>;
+  using const_iterator = _iterator< const stack_pool, stack_type, const value_type>;
 
-  iterator begin(stack_type x){
-    return iterator{this, x};
-  }
-  iterator end_(){
-    return iterator{this,end()};
-  } // this is not a typo
-  //
-  const_iterator begin(stack_type x) const {
-    return const_iterator{this, x};
-  }
-  const_iterator end_() const{
-    return const_iterator{this,end()};
-  }
-  //
-  // const_iterator cbegin(stack_type x) const;
-  // const_iterator cend(stack_type ) const;
 
-  ~stack_pool() {};
+  iterator begin(stack_type x)  { return iterator{this, x}; }
 
-  stack_type new_stack() { 
-    return end();} // return an empty stack---what value shuld return? 1-first free place?
-
-  void reserve(size_type n){ //do not work
-   pool.reserve(n);
-  } // reserve n nodes in the pool
-
-  size_type capacity() const {
-    return pool.capacity();
-  } // the capacity of the pool
-
-  bool empty(stack_type x) const{
-    return x == end();
-  }
-
-  stack_type end() const noexcept { 
-    return stack_type(0); } //0 is default to end
-
-  T& value(stack_type x) {
-    // return reference to top value of stack x
-    return node(x).value;
-  }
-  const T& value(stack_type x) const {
-    // return reference to top value of stack x
-    return node(x).value;
-  }
-  //
-  stack_type& next(stack_type x){
-    return node(x).next;
-  }
-  const stack_type& next(stack_type x) const{
-    return node(x).next;
-  }
+  iterator end(stack_type)  { return iterator{this,end()}; } // this is not a typo
   
-  stack_type push(const T& val, stack_type head){
-    return _push(std::move(val),head);
-  }
-  stack_type push(T&& val, stack_type head){
-     return _push(std::move(val),head);
-  }
-  //
-  stack_type pop(stack_type x){     // what if x is 0 ??
+
+  const_iterator begin(stack_type x) const { return const_iterator{this, x}; }
+
+  const_iterator end(stack_type) const { return const_iterator{this,end()}; }
+  
+
+  const_iterator cbegin(stack_type x) const { return const_iterator{this, x}; }
+
+  const_iterator cend(stack_type) const { return const_iterator{this,end()}; }
+
+
+  stack_type new_stack() noexcept { return end(); } 
+
+  void reserve(size_type n) { pool.reserve(n); }  // reserve n nodes in the pool
+
+  size_type capacity() const noexcept { return pool.capacity(); } // the capacity of the pool
+
+  bool empty(stack_type x) const noexcept { return x == end(); }
+
+  stack_type end() const noexcept { return stack_type(0); } //0 is default to end
+
+
+  T& value(stack_type x) noexcept { return node(x).value; }
+
+  const T& value(stack_type x) const noexcept { return node(x).value;  }
+
+
+  stack_type& next(stack_type x) noexcept { return node(x).next;  }
+
+  const stack_type& next(stack_type x) const noexcept { return node(x).next;  }
+  
+
+  stack_type push(const T& val, stack_type head) { return _push(val,head);  }
+
+  stack_type push(T&& val, stack_type head) { return _push(std::move(val),head); }
+
+
+  stack_type pop(stack_type x) {    
+    // remove first node of a stack 
+    if( empty(x) == true) { //if empty nothing to do
+      return x;
+    }
     stack_type tmp = x;
-    x = next(x);
-    next(tmp) = free_nodes;
+    x = next(x);      //new head
+    next(tmp) = free_nodes;   // old head become new head of free_nodes
     free_nodes = std::move(tmp);
     return x;
-  } // delete first node
-  //
+  } // remove first node
+
   stack_type free_stack(stack_type x){
-    for (auto ptr = begin(x); ptr != end_(); ++ptr){
-      pop(ptr);
-    } 
+    while(x != end()){
+      x = pop(x);
+    }
     return x;
   } // free entire stack
 
-  void print_stack(stack_type x){
-    for (auto ptr = begin(x); ptr != end_(); ++ptr){
+  // stack_type push_stack(stack_type x, stack_type y){
+  //   // push stack x to y
+  //   if(empty(y) == true ) {    // if y is empty
+  //     return x;                  // y become x
+  //   }else{
+  //     stack_type tmp = x;        
+  //     for (auto ptr = cbegin(next(tmp)); ptr != cend(0); ++ptr){
+  //       tmp = next(tmp);
+  //     } 
+  //     next(tmp) = y;    //last element of x "point" to old y
+  //     return x;
+  //   }
+  // }
+
+  void print_stack(stack_type x)  {
+    for (auto ptr = cbegin(x); ptr != cend(0); ++ptr)
+    {
       std::cout << *ptr  << "\n" ;
     } 
   }
@@ -163,20 +163,26 @@ class stack_pool{
 int main(int argc, char const *argv[]) {
   stack_pool<int, std::size_t> pool{};
   
-  auto l = pool.new_stack(), f = pool.new_stack();
-  pool.reserve(10);
+  auto l = pool.new_stack(), g=pool.new_stack();
+  // l = pool.push(1,l);
+  // l = pool.push(2,l);
+  g = pool.push(102,g);
+  g = pool.push(103,g);
+  // pool.print_stack(l);
+  // pool.print_stack(g);
+  // g = pool.push_stack(l,g);
+  // pool.print_stack(g);
+  g=pool.free_stack(g);
+  pool.print_stack(g);
+  // auto y = x;
+  // std::cout << (x == y) << "\n";
+  // // std::cout << x << "\n";
+  // std::cout << *x << "\n";
+  // std::cout << *y << "\n";
+  
+  
 
-  for(int i=0; i<10; i++){
-  l = pool.push(i,l);
-  f = pool.push(10*i,f);
-  }
-  l = pool.push(10,l);
-  pool.print_stack(l);
-  pool.print_stack(f);
-  // std::cout << pool.value(l) << "\n";
-  // l = pool.pop(l);
-  // // l = pool.pop(l);
-  // std::cout << pool.value(l) << "\n";
+  
   // // l == std::size_t(1)
   // pool.value(l) = 77;
   // return 0;
